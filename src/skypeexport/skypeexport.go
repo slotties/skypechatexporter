49e6dd6b9f0a53e0bfb4999603dcc148237f0d78
Stefan Lotties <slotties@gmail.com>
2014-09-15 22:27:37 +0200

    auto-locate main.db (currently just for win/desktop)

--- src/skypeexport/skypeexport.go
+++ src/skypeexport/skypeexport.go
@@ -7,10 +7,13 @@ import (
 	"time"
 	"flag"
 	"os"
+	"os/user"
+	"errors"
+	"io/ioutil"
 )
 
 func main() {
-	var dbFileName = flag.String("db", "main.db", "Path to the database file")
+	var dbFileNameParam = flag.String("db", "", "Path to the database file")
 	var chatName = flag.String("chatname", "", "Path to the database file")
 	flag.Parse()
 
@@ -19,14 +22,67 @@ func main() {
 		os.Exit(11)
 	}
 
-	err := dumpLogs(*dbFileName, *chatName)
+	dbFileName, err := locateDatabase(*dbFileNameParam)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(12)
+	}
+
+	err = dumpLogs(dbFileName, *chatName)
 
 	if err != nil {
-		// FIXME: respond with an error code beside 0 here
 		fmt.Println(err)
 		os.Exit(1)
 	} else {
 		os.Exit(0)
+	}
+}
+
+func locateDatabase(providedFileName string) (string, error) {
+	if providedFileName != "" {
+		_, err := os.Stat(providedFileName)
+		return providedFileName, err
+	}
+
+	// Search for main.db in various locations:
+	currentUser, _ := user.Current()
+	homeDir := currentUser.HomeDir
+	possibleLocations := []string {
+		// Skype on Windows for Desktop
+		homeDir + "/AppData/Roaming/Skype",
+		// Skype on Windows 8 (App)
+		// TODO
+		// Skype on Linux
+		// TODO
+	}
+
+	for _, dirName := range possibleLocations {
+		// Search for a main.db in the account directories.
+		files, err := ioutil.ReadDir(dirName)
+		// Skip any non-existent directory.
+		if err == nil {
+			for _, file := range files {
+				mainDbFile, exists := containsMainDb(dirName, file)
+				if exists {
+					return mainDbFile, nil
+				}
+			}
+		}
+	}
+
+	return "", errors.New("Could not locate main.db")
+}
+
+func containsMainDb(rootDir string, dir os.FileInfo) (string, bool) {
+	if dir.IsDir() {
+		mainDbFile := rootDir + "/" + dir.Name() + "/main.db"
+		if _, err := os.Stat(mainDbFile); os.IsNotExist(err) {
+			return "", false
+		} else {
+			return mainDbFile, true
+		}
+	} else {
+		return "", false
 	}
 }
 
